ab0c9687ebe44e9dea46e592a8214d8b5e784779
Alexander Shelepov <sasha.shelepov@gmail.com>
2014-03-18 18:42:22 +0700

    Added PHPUnit

---
+++ tests/AllTests.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexander
+ * Date: 3/18/14
+ * Time: 6:17 PM
+ */
+
+namespace tests;
+
+use PHPUnit_Framework_TestSuite;
+
+class AllTests extends PHPUnit_Framework_TestSuite
+{
+    public static function suite()
+    {
+
+    }
+}
+ 
