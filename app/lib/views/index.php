d03daf7f56d651b6fa287c33dc22e2fc0bce0b57
Alexander Shelepov <sasha.shelepov@gmail.com>
2014-03-22 21:27:19 +0700

    Library: Changed access

--- app/lib/views/index.php
+++ app/lib/views/index.php
@@ -1,12 +1,17 @@
 <div class="col col-4">
     <div class="container">
-        <a href="/lib/addcategory" class="add-btn">Добавить категорию</a>
+        <?php if ($globals["user"]->hasRole('Библиотекарь')): ?>
+            <a href="/lib/addcategory" class="add-btn">Добавить категорию</a>
+        <?php endif; ?>
 
         <h3>Категории</h3>
         <ul class="library-category-list">
-        <?php foreach ($params["categories"] as $category): ?>
-            <li><a href="/lib/category/<?php echo $category["id"]; ?>" title="<?php echo $category["title"]; ?>"><?php echo $category["title"]; ?></a></li>
-        <?php endforeach; ?>
+            <?php foreach ($params["categories"] as $category): ?>
+                <li>
+                    <a href="/lib/category/<?php echo $category["id"]; ?>"
+                       title="<?php echo $category["title"]; ?>"><?php echo $category["title"]; ?></a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </div>
 </div>
