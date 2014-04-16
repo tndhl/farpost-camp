1a5a8b83f551b4895ea6fd3fcac9d4d701708aa2
Alexander Shelepov <sasha.shelepov@gmail.com>
2014-04-16 21:41:01 +0700

    Изменение меню

--- templates/default.html.php
+++ templates/default.html.php
@@ -25,8 +25,7 @@
         <div class="layout">
             <ul>
                 <li class="active"><a href="/" title="Главная">Главная</a></li>
-                <li><a href="/news" title="Новости и уведомления">Новости</a></li>
-                <li><a href="/" title="Документы">Документы</a></li>
+                <li><a href="/news" title="Новости и уведомления">Новости и уведомления</a></li>
                 <li><a href="/disk" title="Файлообменник">Файлообменник</a></li>
                 <li><a href="/lib" title="Библиотека">Библиотека</a></li>
                 <li><a href="/" title="Форум">Форум</a></li>
